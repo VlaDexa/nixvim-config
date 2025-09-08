c57e6399898011d4301a268a8ec6c506d763c50f
Vladislav Grechannik <vgrechannik@gmail.com>
2025-09-08 19:00:51 +0200

    Add commands to disable formatting

--- config/plugins/conform.nix
+++ config/plugins/conform.nix
@@ -53,4 +53,30 @@
       options.desc = "[F]ormat buffer";
     }
   ];
+
+  userCommands = {
+    "FormatDisable" = {
+      command = lib.nixvim.mkRaw ''
+        function(args)
+          if args.bang then
+            -- FormatDisable! will disable formatting just for this buffer
+            vim.b.disable_autoformat = true
+          else
+            vim.g.disable_autoformat = true
+          end
+        end
+      '';
+      desc = "Disable autoformat-on-save";
+      bang = true;
+    };
+    "FormatEnable" = {
+      command = lib.nixvim.mkRaw ''
+        function()
+          vim.b.disable_autoformat = false
+          vim.g.disable_autoformat = false
+        end
+      '';
+      desc = "Re-enable autoformat-on-save";
+    };
+  };
 }
